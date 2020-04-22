9d0f9499f5dea8205894fe2f621ca2fd3ebb69a8
Matheus Lino <7matheuslino@gmail.com>
2020-04-21 23:15:55 -0300

    Adding frontend of Repositories API

---
+++ frontend/src/App.js
@@ -0,0 +1,53 @@
+import React, { useEffect, useState } from "react";
+
+import "./styles.css";
+import api from "./services/api";
+
+function App() {
+
+  const [repositories, setRepositories] = useState([])
+
+  useEffect(() => {
+    api.get('repositories').then(response => {
+      setRepositories(response.data)
+    })
+  }, [])
+
+  async function handleAddRepository() {
+    const response = await api.post('repositories', {
+      title: `Novo Repositório ${Date.now()}`,
+      url: "",
+      techs: []
+    })
+
+    setRepositories([...repositories, response.data])
+  }
+
+  async function handleRemoveRepository(id) {
+    api.delete(`repositories/${id}`)
+    
+    const newRepository = repositories.filter(
+      repository => repository.id !== id
+    )
+
+    setRepositories(newRepository)
+
+  }
+
+  return (
+    <div>
+      <ul data-testid="repository-list">
+        {repositories.map(repository => (
+          <li key={repository.id}>
+            {repository.title}
+            <button onClick={() => handleRemoveRepository(repository.id)}>Remover</button>
+          </li>
+        ))}
+      </ul>
+
+      <button onClick={handleAddRepository}>Adicionar</button>
+    </div>
+  );
+}
+
+export default App;
